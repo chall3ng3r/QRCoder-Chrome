16dfca3e485d53281c0645c463d81a2b7eb7c1c6
Faisal Iqbal <fsd.news@orison.biz>
2019-03-09 21:34:36 +0500

    Initial commit
    Chrome browser version.

---
+++ README.md
@@ -0,0 +1,20 @@
+# QRCoder for Chrome
+Quickly get QR Code of currently open page. Google Chrome version.
+
+My original idea of quickly getting QR code from the currently open page was using JS code in bookmark, published on my blog at https://www.orison.biz/blogs/chall3ng3r/2018/05/qr-coder-version-2/
+
+Now newer versions of browsers don't allow this specifically on mobile devices. So, I finally decided to write my first browser extension. 
+
+In version 2.0, I added in-browser QR generator, so it will not send your data to any service, and the QR code is generated really fast.
+
+Ver 1.0:
+Uses online web service to generate QR code
+
+Ver 2.0:
+Offline QR Code Generator for JavaScript by Kazuhiko Arase http://www.d-project.com/
+
+Credits:
+Icon by: Freepik from https://www.flaticon.com/ is licensed by http://creativecommons.org/licenses/by/3.0/
+
+ToDo:
+Add Opera and Safari support.
---
+++ popup/qrcoder.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const [ QR_VERSION_MIN, QR_VERSION_MAX ] = [ 1, 40 ];
+const DEFAULT_ERROR_CORRECTION_LEVEL = 'L'; // 'L', 'M', 'Q', 'H'
+const [ DEFAULT_CELL_SIZE, DEFAULT_MARGIN_SIZE] = [4, 0 ];
+
+function generateQR(text, cellSize=1, margin=0) {
+  for (let i = QR_VERSION_MIN; i <= QR_VERSION_MAX; ++i) {
+    let qr = qrcode(i, DEFAULT_ERROR_CORRECTION_LEVEL);
+    qr.addData(text);
+
+    try {
+      qr.make();
+    } catch (e) {
+      continue;
+    }
+
+    return qr.createImgTag(cellSize, margin);
+    console.debug(str);
+    return str;
+  }
+
+  throw "QR code not available";
+}
+
+chrome.tabs.query({active: true, currentWindow: true}, (tabs => {
+    let tag = generateQR(tabs[0].url, DEFAULT_CELL_SIZE, DEFAULT_MARGIN_SIZE);
+    let doc = new DOMParser().parseFromString(tag, 'text/html');
+    let img_el = doc.getElementsByTagName('img')[0];
+    document.getElementById('qr').src = img_el.src;
+}));
